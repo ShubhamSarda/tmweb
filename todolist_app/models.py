5d761d7e4833d33fc62cf60f505b568b9b4e31c9
shubham <shubhamsarda99@yahoo.com>
2019-07-19 19:29:33 +0530

    Uploading Project

---
+++ todolist_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class TaskList(models.Model):
+    manage = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    task = models.CharField(max_length=300)
+    done = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.task + " - " + str(self.done)
