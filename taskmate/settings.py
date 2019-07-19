8587849fb61e1854cf9b7964cbba6334a45c1f46
shubham <shubhamsarda99@yahoo.com>
2019-07-19 20:07:05 +0530

    Updated Static Files

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
